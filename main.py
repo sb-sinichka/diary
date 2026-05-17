6036f0b4bd7fc8b832d2ce133c9594ddaad8c992
sb-sinichka <p81951554@gmail.com>
2026-05-17 18:54:51 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,92 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
 #Импорт
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request, redirect, session
 #Подключение библиотеки баз данных
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+#Задаем секретный ключ для работы session
+app.secret_key = 'my_top_secret_123'
 #Подключение SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Создание db
-db = SQLAlchemy(app )
-
-#Задание №1. Создай таблицу БД
+db = SQLAlchemy(app)
+#Создание таблицы
 
 class Card(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str]
-    subtitle: Mapped[str]
-    text: Mapped[str]
+    #Создание полей
+    #id
+    id = db.Column(db.Integer, primary_key=True)
+    #Заголовок
+    title = db.Column(db.String(100), nullable=False)
+    #Описание
+    subtitle = db.Column(db.String(300), nullable=False)
+    #Текст
+    text = db.Column(db.Text, nullable=False)
+    #email владельца карточки
+    user_email = db.Column(db.String(100), nullable=False)
 
+    #Вывод объекта и id
+    def __repr__(self):
+        return f'<Card {self.id}>'
+    
 
-
-
-
-
-
-
+#Задание №1. Создать таблицу User
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.String(100),nullable=False)
 
 
 #Запуск страницы с контентом
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def login():
+    error = ''
+    if request.method == 'POST':
+        form_login = request.form['email']
+        form_password = request.form['password']
+            
+        #Задание №4. Реализовать проверку пользователей
+        users = User.query.all()
+        for user in users:
+            if user.email == form_login and user.password == form_password:
+                session ['user.email'] = form_login
+                return redirect('/index')
+     
+    else:
+        return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        #Задание №3. Реализовать запись пользователей
+        user = User(email=email,password=password)
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+#Запуск страницы с контентом
+@app.route('/index')
 def index():
-    #Отображение объектов из БД
-    #Задание №2. Отоброзить объекты из БД в index.html
-    cards = Card.query.order_by(Card.id.desc()).all()
-
-    return render_template('index.html',
-                           cards = cards
-
-                           )
+    #Задание №4. Сделай, чтобы пользователь видел тольуо свои карточки
+    cards = Card.query.filter_by(user_email=session['user.email']).order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
 #Запуск страницы c картой
 @app.route('/card/<int:id>')
 def card(id):
-    #Задание №2. Отоброзить нужную карточку по id
     card = Card.query.get(id)
-    
 
     return render_template('card.html', card=card)
 
@@ -64,18 +103,14 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        #Задание №2. Создайте сопосб записи данных в БД
-        card = Card(title = title,subtitle = subtitle,text = text)
+        #Задание №4. Сделай, чтобы создание карточки происходило от имени пользователя
+        card = Card(title=title, subtitle=subtitle, text=text, user_email=session['user.email'])
+
         db.session.add(card)
         db.session.commit()
-
-
-
-
-        return redirect('/')
+        return redirect('/index')
     else:
         return render_template('create_card.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
